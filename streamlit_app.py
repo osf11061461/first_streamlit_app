efd548cd1aa6a438b371259bdfb02aca15a6ecbf
osf11061461 <149007849+osf11061461@users.noreply.github.com>
2023-10-28 18:52:58 +0530

    Update streamlit_app.py
    u

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_cur.execute("select initcap(FRUIT_NAME) as FRUIT_NAME from pc_rivery_db.public.fruit_load_list;")
 my_data_row = my_cur.fetchone()
 streamlit.header("Fruit Load List Contains:")
 streamlit.dataframe(my_data_row)
